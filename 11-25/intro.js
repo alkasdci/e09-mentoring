affbe7bffdf7ed01ac1b154956121a875e68ec7c
alkasdci <alkas.alkiviadis-freelancer@digitalcareerinstitute.org>
2022-11-25 11:46:16 +0200

    intro to hs

---
+++ 11-25/intro.js
@@ -0,0 +1,85 @@
+/* 
+    multiline comment
+*/
+
+/**
+ * sdfgsdfg
+ * sdfg
+ * sdfg
+ * 
+ */
+
+// this is a single line comment
+
+// weather is the variable name
+// "=" is an operator that ASSIGNS the value at the right side of it to the left of it
+// "sunny" is a value
+let weather = "sunny" // 'sunny' `sunny`
+
+weather = 'rainy'
+weather = 'snowy'
+
+let temperature = 15 // this is a number
+
+let username = 'alkas56'
+
+let streetnumber = '52' // string
+
+let wrongQuotes = "'''''sdf'"
+let name = 'ifeoma'
+let lastname = "doe"
+
+let smoker = true
+let pregnat = false
+
+const pi = 3.14
+// pi = 5
+console.log(temperature * 4)
+
+// 1 + 1 = 2 :> "+" and "=" are operators
+// "1" and "2" are operands
+
+// epxressions
+// is a piece of code that id being calculated and produces a result
+
+console.log(2 * 2 - 2 * 10)
+console.log(name + " " + lastname)
+
+console.log(myName) // hoisting. no error
+
+var myName = 'alkis'
+
+
+
+const hisName = 'Ion'
+
+// hisName = 'jim'
+console.log(hisName)
+
+// console.log(yourName) // hoisting
+let yourName; // NOT INITIALIZED
+
+yourName = 'oksana' 
+
+yourName = 'mandy'// CAN REASSIGN VALUES 
+console.log("🚀 ~ yourName", yourName)
+
+var anotherName;
+
+anotherName = 'Alvaro'
+console.log("🚀 ~ anotherName", anotherName)
+
+// block of code lines 74-76
+{
+    let age = 5
+    console.log("🚀 ~ age", age)
+    
+}
+
+
+// console.log("🚀 ~ age", age)
+
+jkhafdjklh = 'akjsdhfjkdskhjsfd'
+jkhafdjklh = 3
+console.log("🚀 ~ jkhafdjklh", jkhafdjklh)
+
